615bf6ccd1bbd4c4a8634ee0a8c971ef32256cfd
Marko Šatlan <markosatlan@gmail.com>
2021-02-26 09:30:40 +0100

    Update file structure

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,5 @@ export { default as Footer } from './common/FooterComponent';
 export { Loading } from './common/LoadingComponent';
 export { default as Contact } from './contact/ContactComponent';
 export { default as DishDetail } from './detail/DishdetailComponent';
-export { default as Home } from './home/withHooks/Home';
+export { default as Home } from './home/Home';
 export { default as Menu } from './menu/Menu';
